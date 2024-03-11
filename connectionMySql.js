bd7e5366e89975beff2566fd58972d8fc16c3aef
sebastianmarius <h.sebiastian@gmail.com>
2024-03-11 15:18:19 +0200

    clean up

---
+++ connectionMySql.js
@@ -0,0 +1,34 @@
+// Import dotenv and load environment variables from .env file
+require('dotenv').config();
+
+const mysql = require('mysql2');
+
+// Here we create the connection to the database
+const connection = mysql.createConnection({
+  host: process.env.host,
+  user: process.env.user,
+  port: process.env.port,
+  password: process.env.password,
+  database: process.env.database
+}); 
+
+// Connect to the database
+connection.connect(function (err) {
+  if (err) {
+    console.error('Login error:', err);
+    return;
+  }
+  console.log('Successfully connected to MySQL database!');
+
+  // Execute query
+  connection.query('SELECT * FROM users', function (error, results, fields) {
+    if (error) {
+      console.error('Error executing the query:', error);
+      return;
+    }
+    console.log('Query results:', results);
+  });
+
+  // Close the connection when done
+  connection.end();
+});
