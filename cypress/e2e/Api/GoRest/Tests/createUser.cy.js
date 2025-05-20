1d0543c71da99dab679a10685244cddbff399258
SebastianM25 <h.sebiastian@gmail.com>
2025-05-20 20:09:16 +0300

    i have created env variables

--- cypress/e2e/Api/GoRest/Tests/createUser.cy.js
+++ cypress/e2e/Api/GoRest/Tests/createUser.cy.js
@@ -1,52 +1,34 @@
 describe('API Testing for User Creation', () => {
   it('Create user and verify data', () => {
-    // Here generate random email and id
     const email = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(6, 15);
-    const id = Math.floor((Math.random() * 1000) + 1);
-    const bearerToken='22b20f46d9bf87f782edcef4d5676b00c4df839914b990ae1a1ce94db0317572';
+    const bearerToken = Cypress.env('bearerToken');
+    const gorestBaseUrl = Cypress.env('gorestBaseUrl');
 
-    // Save email and id as Cypress global variables
     cy.wrap(email).as('generatedEmail');
-    
-    cy.wrap(id).as('generatedId');
 
-    // Make POST request to create user
     cy.request({
       method: 'POST',
-      url: 'https://gorest.co.in/public/v2/users',
+      url: `${gorestBaseUrl}/users`,
       headers: {
-          'Authorization': `Bearer ${bearerToken}`,
-          'Content-Type': 'application/json', 
-         
+        'Authorization': `Bearer ${bearerToken}`,
+        'Content-Type': 'application/json'
       },
       body: {
         name: 'SebaTest2',
         email: `sebbtest${email}@block.io`,
         gender: 'male',
-        status: 'inactive'
+        status: 'active'
       }
     }).then((response) => {
-      // Ensure the request was successful
       expect(response.status).to.eq(201);
 
-      // Check if the response contains user data
-      if (response.body && response.body.hasOwnProperty('data')) {
-        // Verify the created user's data
-        expect(response.body.data.name).to.eq('SebaTest5');
-        expect(response.body.data.email).to.eq(`sebbtest${email}@block.io`);
-        expect(response.body.data.gender).to.eq('male');
-        expect(response.body.data.status).to.eq('inactive');
-      } else {
-        // Log an error message if user data is not found in the response
-        cy.log('User data not found in API response.');
-      }
+      expect(response.body.name).to.eq('SebaTest2');
+      expect(response.body.email).to.eq(`sebbtest${email}@block.io`);
+      expect(response.body.gender).to.eq('male');
+      expect(response.body.status).to.eq('active');
 
-      // Access Cypress global variables for email and id
       cy.get('@generatedEmail').then((generatedEmail) => {
-        cy.get('@generatedId').then((generatedId) => {
-          cy.log(`Generated email: ${generatedEmail}`);
-          cy.log(`Generated id: ${generatedId}`);
-        });
+        cy.log(`Generated email: ${generatedEmail}`);
       });
     });
   });
